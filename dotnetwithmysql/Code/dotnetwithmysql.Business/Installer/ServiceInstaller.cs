068770885b259e547f92cbdfd8d493adde05cb6b
PasupulaS-Hexaware <1000071061@hexaware.com>
2023-02-27 09:29:48 +0000

    boilerplatecode generation

---
+++ dotnetwithmysql/Code/dotnetwithmysql.Business/Installer/ServiceInstaller.cs
@@ -0,0 +1,25 @@
+using dotnetwithmysql.Data.Installer;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+namespace dotnetwithmysql.Business.Installer
+{
+    public class ServiceInstaller
+    {
+        private IServiceCollection _service;
+        public ServiceInstaller(IServiceCollection service)
+        {
+            _service = service;
+        }
+
+        public void Install()
+        {
+            _service.Scan(scan => scan
+                                    .FromAssemblyOf<ServiceInstaller>()
+                                    .AddClasses()
+                                    .AsImplementedInterfaces()
+                                    .WithScopedLifetime());
+            var dataInstaller = new DataInstaller(_service);
+            dataInstaller.Install();
+        }
+    }
+}
